0d6ffd848ad364b9abb0e364593a35927f81b267
Artem <artem@Ubuntu.myguest.virtualbox.org>
2023-09-27 00:23:07 +0500

    Update main.py

--- homework_01/main.py
+++ homework_01/main.py
@@ -4,29 +4,32 @@
 """
 
 
-def power_numbers():
+def power_numbers(*numbers):
     """
     функция, которая принимает N целых чисел,
     и возвращает список квадратов этих чисел
     >>> power_numbers(1, 2, 5, 7)
     <<< [1, 4, 25, 49]
     """
-
+    return [number ** 2 for number in numbers]
 
 # filter types
 ODD = "odd"
 EVEN = "even"
 PRIME = "prime"
 
+def is_prime(n):
 
-def filter_numbers():
+def filter_numbers(number_list, filter_type):
     """
     функция, которая на вход принимает список из целых чисел,
-    и возвращает только чётные/нечётные/простые числа
-    (выбор производится передачей дополнительного аргумента)
-
-    >>> filter_numbers([1, 2, 3], ODD)
-    <<< [1, 3]
+    и возвращает только чётные/нечётные/простые числа:
     >>> filter_numbers([2, 3, 4, 5], EVEN)
     <<< [2, 4]
     """
+    if (filter_type == ODD):
+        return [number for number in number_list if number % 2 != 0]
+    if (filter_type == EVEN):
+        return [number for number in number_list if number % 2 == 0]
+    if (filter_type == PRIME):
+        return [number for number in number_list if is_prime(number) is True]
