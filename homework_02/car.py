7fa45fad93961e33ec8f37f7fe0b1083813824b3
Artem <artem@Ubuntu.myguest.virtualbox.org>
2023-10-19 21:33:59 +0500

    Add homework_02

--- homework_02/car.py
+++ homework_02/car.py
@@ -1,3 +1,20 @@
 """
 создайте класс `Car`, наследник `Vehicle`
+в модуле `car` создайте класс `Car`
+    - класс `Car` должен быть наследником `Vehicle`
+    - добавьте атрибут `engine` классу `Car`
+    - объявите метод `set_engine`, который принимает в себя экземпляр объекта `Engine' и устанавливает на текущий экземпляр `Car`
 """
+
+from homework_02.base import Vehicle
+from homework_02.engine import Engine
+
+
+class Car(Vehicle):
+    def __init__(self, weight, fuel, fuel_consumption):
+        super().__init__(weight, fuel, fuel_consumption)
+        self.engine = None
+
+    def set_engine(self, engine):
+        if isinstance(engine, Engine):
+            self.engine = engine
--- homework_02/exceptions.py
+++ homework_02/exceptions.py
@@ -4,3 +4,30 @@
 - NotEnoughFuel
 - CargoOverload
 """
+
+
+class LowFuelError(Exception):
+    def __init__(self, *args):
+        self.message = None
+
+    def __str__(self):
+        if self.message:
+            return 'LowFuelError'
+
+
+class NotEnoughFuel(Exception):
+    def __init__(self, *args):
+        self.message = None
+
+    def __str__(self):
+        if self.message:
+            return 'NotEnoughFuel'
+
+
+class CargoOverload(Exception):
+    def __init__(self, *args):
+        self.message = None
+
+    def __str__(self):
+        if self.message:
+            return 'CargoOverload'
